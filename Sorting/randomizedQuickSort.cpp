635259640827fa61d8492430dd25bc2af74337cf
rupak-20 <rupakswami43887@gmail.com>
2021-10-13 00:00:05 +0530

    T: O(n*n), S: O(1)

---
+++ Sorting/randomizedQuickSort.cpp
@@ -0,0 +1,72 @@
+//Randomized Quick Sort using Hoare's partition scheme
+
+//worst case time complexity O(n*n)
+//average time complexity O(nlogn)
+//better than quick sort with last element as pivot
+
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+using namespace std;
+
+//make partitions based on the random pivots
+int partition(vector<int> &arr, int start, int stop) {
+    int pivot = start;
+    int i = start-1, j = stop+1;
+
+    while(true) {
+        while(true) {
+            i++;
+            if(arr[i] >= arr[pivot]) break;
+        }
+        while(true) {
+            j--;
+            if(arr[j] <= arr[pivot]) break;
+        }
+        if(i >= j) return j;
+
+        //swap elements at index i and j
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+//generating random pivot points
+int randPivot(vector<int> &arr, int start, int stop) {
+    //random numbers between stop/4 and 3*stop/4
+    int pivot = (rand()%(3*stop/4 - stop/4 + 1)) + stop/4;
+
+    //swap numbers at index start and pivot
+    int temp = arr[start];
+    arr[start] = arr[pivot];
+    arr[pivot] = temp;
+
+    return partition(arr, start, stop);
+}
+
+//quick sort function
+void quickSort(vector<int> &arr, int start, int stop) {
+    if(start < stop) {
+        int pivotindex = randPivot(arr, start, stop);
+
+        quickSort(arr, start, pivotindex);
+        quickSort(arr, pivotindex + 1, stop);
+    }
+}
+
+
+//driver code
+int main() {
+    vector<int> array = {10, 4, 63, 90, 43, 20, 1, 9};
+    
+    cout<<"unsorted array: ";
+    for(auto x: array) cout<<x<<" ";
+    cout<<"\n";
+    quickSort(array, 0, array.size() - 1);
+    cout<<"sorted array: ";
+    for(auto x: array) cout<<x<<" ";
+    cout<<"\n";
+
+    return 0;
+}
